4c4e4433c9a111c04dc0f5e9ee9fed056e138829
Tiago-22004316 <a22004316@alunos.ulht.pt>
2022-05-04 16:31:55 +0100

    Read me

--- src/main/kotlin/pt/ulusofona/tfc/trabalho/TrabalhoApplication.java
+++ src/main/kotlin/pt/ulusofona/tfc/trabalho/TrabalhoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 @SpringBootApplication
 public class TrabalhoApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(TrabalhoApplication.class, args);
     }
