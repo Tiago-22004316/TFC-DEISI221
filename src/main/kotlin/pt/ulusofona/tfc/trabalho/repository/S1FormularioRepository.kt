5d8c5625b4f5bced291ceebcd5d8d4dbf032ef8a
tomas-21904341 <a21904341@alunos.ulht.pt>
2022-05-04 17:10:40 +0100

    Tentativa de filtrar

--- src/main/kotlin/pt/ulusofona/tfc/trabalho/repository/S1FormularioRepository.kt
+++ src/main/kotlin/pt/ulusofona/tfc/trabalho/repository/S1FormularioRepository.kt
@@ -6,5 +6,6 @@ import pt.ulusofona.tfc.trabalho.dao.S1Formulario
 interface S1FormularioRepository: JpaRepository<S1Formulario, Long> {
 
     fun findByProcessId(processId : String) : S1Formulario?
+    fun findAllByProcessIdContains(processId : String) : S1Formulario?
 
 }
